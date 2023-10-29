4cb8c7f214e6a676476aadafb87bc89c3ddfb0b6
Soraku7 <2673140287@qq.com>
2023-10-30 00:50:50 +0800

    more main

--- src/CreateWindow/CreateWindow.cpp
+++ src/CreateWindow/CreateWindow.cpp
@@ -1,9 +1,14 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+//
+// Created by Soraku7 on 2023/10/29.
+//
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 #include <iostream>
 
+//随着窗体大小更变
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+//读取输入
 void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 800;
@@ -15,7 +20,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -25,28 +30,32 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
+
     //初始化GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    
+
     //重复渲染
     while (!glfwWindowShouldClose(window))
     {
+        //输入
         processInput(window);
+
+        //渲染 交换缓冲
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     glfwTerminate();
     return 0;
 }
 
 void processInput(GLFWwindow *window)
 {
+    //按下ESC关闭窗口
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
