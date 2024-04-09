573329724ca92fde44190e97c802666270efcb75
Soraku7 <2673140287@qq.com>
2024-04-09 17:03:17 +0800

    gitignore

--- src/Coordinate/Cube.cpp
+++ src/Coordinate/Cube.cpp
@@ -191,6 +191,8 @@ int main()
     ourShader.setInt("texture1" , 0);
     ourShader.setInt("texture2" , 1);
     
+    //启用ZBuffer
+    glEnable(GL_DEPTH_TEST);
     //重复渲染
     while (!glfwWindowShouldClose(window))
     {
@@ -198,7 +200,7 @@ int main()
         processInput(window);
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         //机会绑定渲染贴图
         glActiveTexture(GL_TEXTURE0);
